3666f5a0257d1176c854be8a3167e2e43aa845db
june0216 <ziyun1612@ewhain.net>
2023-07-14 16:36:58 +0900

    :tada: init project

---
+++ greendev/src/main/java/com/devoceanyoung/greendev/GreendevApplication.java
@@ -0,0 +1,13 @@
+package com.devoceanyoung.greendev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreendevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreendevApplication.class, args);
+	}
+
+}
---
+++ greendev/src/test/java/com/devoceanyoung/greendev/GreendevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devoceanyoung.greendev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreendevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
