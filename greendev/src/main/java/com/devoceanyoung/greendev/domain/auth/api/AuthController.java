03f8b8e3e0683235164e7df98783dd0588ceb447
june0216 <ziyun1612@ewhain.net>
2023-07-22 02:19:18 +0900

    Added AuthController.java : 인증 관련 컨트롤러 레이어 구현

---
+++ greendev/src/main/java/com/devoceanyoung/greendev/domain/auth/api/AuthController.java
@@ -0,0 +1,83 @@
+package com.devoceanyoung.greendev.domain.auth.api;
+
+import static org.springframework.http.HttpHeaders.*;
+
+import java.util.concurrent.TimeUnit;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.devoceanyoung.greendev.domain.auth.AuthUser;
+import com.devoceanyoung.greendev.domain.auth.dto.LoginResDto;
+import com.devoceanyoung.greendev.domain.auth.dto.TokenResDto;
+import com.devoceanyoung.greendev.domain.auth.service.AuthService;
+import com.devoceanyoung.greendev.domain.member.domain.RoleType;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+//http://localhost:8880/login/oauth2/code/naver
+
+@Slf4j
+@RestController
+@RequestMapping("/login/oauth/code")
+@RequiredArgsConstructor
+public class AuthController 현{
+	private final AuthService authService;
+	@Value("${spring.security.oauth2.client.registration.naver.client-id}")
+	private String clientId;
+
+	@Value("${spring.security.oauth2.client.registration.naver.client-secret}")
+	private String clientSecret;
+
+	@GetMapping("/naver")
+	@ResponseBody
+	public ResponseEntity<LoginResDto> naverOAuthRedirect(@RequestParam String code, @RequestParam String state) {
+		// state는 무시하겠습니다.
+		RestTemplate rt = new RestTemplate();
+
+		HttpHeaders accessTokenHeaders = new HttpHeaders();
+		accessTokenHeaders.add("Content-type", "application/x-www-form-urlencoded");
+
+		MultiValueMap<String, String> accessTokenParams = new LinkedMultiValueMap<>();
+		accessTokenParams.add("grant_type", "authorization_code");
+		accessTokenParams.add("client_id", clientId);
+		accessTokenParams.add("client_secret", clientSecret);
+		accessTokenParams.add("code" , code);	// 응답으로 받은 코드
+		accessTokenParams.add("state" , state); // 응답으로 받은 상태
+
+		HttpEntity<MultiValueMap<String, String>> accessTokenRequest = new HttpEntity<>(accessTokenParams, accessTokenHeaders);
+
+		ResponseEntity<String> accessTokenResponse = rt.exchange(
+			"https://nid.naver.com/oauth2.0/token",
+			HttpMethod.POST,
+			accessTokenRequest,
+			String.class
+		);
+		return ResponseEntity.ok()
+			//.header(SET_COOKIE, responseCookie.toString())
+			.body(new LoginResDto("test", accessTokenResponse.getBody()));
+
+	}
+}
