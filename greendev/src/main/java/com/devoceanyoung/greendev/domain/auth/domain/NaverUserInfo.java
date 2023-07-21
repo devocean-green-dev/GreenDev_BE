3b79d4a5069932347a94789ede8cc3a73047a763
june0216 <ziyun1612@ewhain.net>
2023-07-22 04:27:03 +0900

    Update NaverUserInfo : json 형태 파악 후 형태에 맞게 변경

--- greendev/src/main/java/com/devoceanyoung/greendev/domain/auth/domain/NaverUserInfo.java
+++ greendev/src/main/java/com/devoceanyoung/greendev/domain/auth/domain/NaverUserInfo.java
@@ -2,12 +2,20 @@ package com.devoceanyoung.greendev.domain.auth.domain;
 
 import java.util.Map;
 
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import com.devoceanyoung.greendev.domain.member.domain.ProviderType;
+
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class NaverUserInfo implements OAuth2UserInfo{
 
-	private Map<String, Object> attributes;
+	private final Map<String, Object> attributes;
+
+	public NaverUserInfo(OAuth2User oAuth2User) {
+		this.attributes = oAuth2User.getAttribute("response");
+	}
 
 	@Override
 	public String getProviderId() {
@@ -15,8 +23,8 @@ public class NaverUserInfo implements OAuth2UserInfo{
 	}
 
 	@Override
-	public String getProvider() {
-		return "naver";
+	public ProviderType getProvider() {
+		return ProviderType.NAVER;
 	}
 
 	@Override
@@ -26,11 +34,11 @@ public class NaverUserInfo implements OAuth2UserInfo{
 
 	@Override
 	public String getName() {
-		return (String) ((Map) attributes.get("profile")).get("nickname");
+		return (String) attributes.get("nickname");
 	}
 	@Override
 	public String getProfileImageUrl(){
-		return (String) ((Map) attributes.get("profile")).get("profile_image");
+		return (String) attributes.get("profile_image");
 	}
 
 
