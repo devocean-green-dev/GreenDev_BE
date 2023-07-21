45eb30e7d57542abe469d9407013705d0a0cb46c
june0216 <ziyun1612@ewhain.net>
2023-07-22 04:26:43 +0900

    Update KakaoUserInfo : json 형태 파악 후 형태에 맞게 변경

--- greendev/src/main/java/com/devoceanyoung/greendev/domain/auth/domain/KakaoUserInfo.java
+++ greendev/src/main/java/com/devoceanyoung/greendev/domain/auth/domain/KakaoUserInfo.java
@@ -2,12 +2,15 @@ package com.devoceanyoung.greendev.domain.auth.domain;
 
 import java.util.Map;
 
+import com.devoceanyoung.greendev.domain.member.domain.ProviderType;
+
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class KakaoUserInfo implements OAuth2UserInfo{
 
 	private Map<String, Object> attributes;
+
 
 	@Override
 	public String getProviderId() {
@@ -16,24 +19,34 @@ public class KakaoUserInfo implements OAuth2UserInfo{
 	}
 
 	@Override
-	public String getProvider() {
-		return "kakao";
+	public ProviderType getProvider() {
+		return ProviderType.KAKAO;
 	}
 
 	@Override
 	public String getEmail() {
 		// kakao_account라는 Map에서 추출
-		return (String) ((Map) attributes.get("kakao_account")).get("email");
+		//return (String) ((Map) attributes.get("kakao_account")).get("email");
+		//return (String) attributes.get("email");
+		Map<String, Object> kakaoAccount = (Map<String, Object>)attributes.get("kakao_account");
+		return (String) kakaoAccount.get("email");
 	}
 
 	@Override
 	public String getName() {
 		// kakao_account라는 Map에서 추출
-		return (String) ((Map) attributes.get("properties")).get("nickname");
+		//return (String) ((Map) attributes.get("profile")).get("nickname");
+		Map<String, Object> kakaoAccount = (Map<String, Object>)attributes.get("kakao_account");
+		Map<String, Object> kakaoProfile = (Map<String, Object>)kakaoAccount.get("profile");
+		return (String) kakaoProfile.get("nickname");
 	}
 
 	@Override
 	public String getProfileImageUrl(){
-		return (String) ((Map) attributes.get("properties")).get("profile_image");
+
+		//return (String) ((Map) attributes.get("profile")).get("profile_image_url");
+		Map<String, Object> kakaoAccount = (Map<String, Object>)attributes.get("kakao_account");
+		Map<String, Object> kakaoProfile = (Map<String, Object>)kakaoAccount.get("profile");
+		return (String) kakaoProfile.get("profile_image_url");
 	}
 }
