cd2b694e324e553e59053b2afda7b58e4eb1546b
june0216 <ziyun1612@ewhain.net>
2023-07-22 16:01:06 +0900

    Added AuthUser.java : 현재 인증된 사용자의 세부 정보에 접근

---
+++ greendev/src/main/java/com/devoceanyoung/greendev/domain/auth/AuthUser.java
@@ -0,0 +1,15 @@
+package com.devoceanyoung.greendev.domain.auth;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+@Target({ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@AuthenticationPrincipal(expression = "#this == 'anonymousUser' ? null : member")
+public @interface AuthUser {
+
+}
