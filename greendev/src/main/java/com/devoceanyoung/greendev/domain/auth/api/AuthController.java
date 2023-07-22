a3a3df9eed184ae5c92971a6d88d4c78c99bfb78
june0216 <ziyun1612@ewhain.net>
2023-07-22 17:11:51 +0900

    Update AuthController : 유저 정보 가져오는 테스트용 api 추가

--- greendev/src/main/java/com/devoceanyoung/greendev/domain/auth/api/AuthController.java
+++ greendev/src/main/java/com/devoceanyoung/greendev/domain/auth/api/AuthController.java
@@ -33,6 +33,7 @@ import com.devoceanyoung.greendev.domain.auth.AuthUser;
 import com.devoceanyoung.greendev.domain.auth.dto.LoginResDto;
 import com.devoceanyoung.greendev.domain.auth.dto.TokenResDto;
 import com.devoceanyoung.greendev.domain.auth.service.AuthService;
+import com.devoceanyoung.greendev.domain.member.domain.Member;
 import com.devoceanyoung.greendev.domain.member.domain.RoleType;
 
 import lombok.RequiredArgsConstructor;
@@ -43,9 +44,9 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping("/login/oauth/code")
 @RequiredArgsConstructor
-public class AuthController 현{
+public class AuthController {
 	private final AuthService authService;
-	@Value("${spring.security.oauth2.client.registration.naver.client-id}")
+	@Value("${spring.security.oauth2.client.registration.kakao.client-id}")
 	private String clientId;
 
 	@Value("${spring.security.oauth2.client.registration.naver.client-secret}")
@@ -54,6 +55,35 @@ public class AuthController 현{
 	@GetMapping("/naver")
 	@ResponseBody
 	public ResponseEntity<LoginResDto> naverOAuthRedirect(@RequestParam String code, @RequestParam String state) {
+
+		RestTemplate rt = new RestTemplate();
+
+		HttpHeaders accessTokenHeaders = new HttpHeaders();
+		accessTokenHeaders.add("Content-type", "application/x-www-form-urlencoded");
+
+		MultiValueMap<String, String> accessTokenParams = new LinkedMultiValueMap<>();
+		accessTokenParams.add("grant_type", "authorization_code");
+		accessTokenParams.add("client_id", clientId);
+		accessTokenParams.add("client_secret", clientSecret);
+		accessTokenParams.add("code" , code);	// 응답으로 받은 코드
+		accessTokenParams.add("state" , state); // 응답으로 받은 상태
+
+		HttpEntity<MultiValueMap<String, String>> accessTokenRequest = new HttpEntity<>(accessTokenParams, accessTokenHeaders);
+
+		ResponseEntity<String> accessTokenResponse = rt.exchange(
+			"https://nid.naver.com/oauth2.0/token",
+			HttpMethod.POST,
+			accessTokenRequest,
+			String.class
+		);
+		return ResponseEntity.ok()
+			//.header(SET_COOKIE, responseCookie.toString())
+			.body(new LoginResDto("test", accessTokenResponse.getBody()));
+
+	}
+	@GetMapping("/kakao")
+	@ResponseBody
+	public ResponseEntity<LoginResDto> kakaoOAuthRedirect(@RequestParam String code, @RequestParam String state) {
 		// state는 무시하겠습니다.
 		RestTemplate rt = new RestTemplate();
 
@@ -78,6 +108,13 @@ public class AuthController 현{
 		return ResponseEntity.ok()
 			//.header(SET_COOKIE, responseCookie.toString())
 			.body(new LoginResDto("test", accessTokenResponse.getBody()));
+
+	}
+
+	@GetMapping("/test")
+	@ResponseBody
+	public ResponseEntity<String> test(@AuthUser Member member){
+		return ResponseEntity.ok().body(member.getEmail());
 
 	}
 }
