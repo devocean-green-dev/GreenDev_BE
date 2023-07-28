e66bce19529df9a8a4fc553392fa70472fa5ade6
june0216 <ziyun1612@ewhain.net>
2023-07-29 03:10:14 +0900

    Update MemberService : 닉네임 변경 로직 추가

--- greendev/src/main/java/com/devoceanyoung/greendev/domain/member/service/MemberService.java
+++ greendev/src/main/java/com/devoceanyoung/greendev/domain/member/service/MemberService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devoceanyoung.greendev.domain.member.domain.Member;
+import com.devoceanyoung.greendev.domain.member.dto.MemberReqDto;
 import com.devoceanyoung.greendev.domain.member.repository.MemberRepository;
 import com.devoceanyoung.greendev.global.exception.customException.MemberNotFoundException;
 
@@ -16,6 +17,12 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class MemberService {
 	final private MemberRepository memberRepository;
+
+	public void update(Long memberId, MemberReqDto reqDto) {
+		Member member = memberRepository.findById(memberId)
+			.orElseThrow(MemberNotFoundException::new);
+		member.updateNickname(reqDto.getNickname());
+	}
 	@Transactional(readOnly = true)
 	public boolean isExistedEmail(String email){
 		return memberRepository.existsByEmail(email);
