0c0a0af662a67ede3ec9bf045e0dbda39934de22
june0216 <ziyun1612@ewhain.net>
2023-07-28 22:58:22 +0900

    Update AuthService : 토큰 재발급, 로그아웃 서비스 로직

--- greendev/src/main/java/com/devoceanyoung/greendev/domain/auth/service/AuthService.java
+++ greendev/src/main/java/com/devoceanyoung/greendev/domain/auth/service/AuthService.java
@@ -1,7 +1,5 @@
 package com.devoceanyoung.greendev.domain.auth.service;
 
-import javax.security.auth.login.AccountNotFoundException;
-
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
@@ -9,11 +7,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.devoceanyoung.greendev.domain.auth.dto.LoginResDto;
-import com.devoceanyoung.greendev.domain.auth.dto.TokenResDto;
-import com.devoceanyoung.greendev.domain.member.domain.Member;
+import com.devoceanyoung.greendev.domain.auth.dto.AccessTokenDto;
 import com.devoceanyoung.greendev.domain.member.repository.MemberRepository;
-import com.devoceanyoung.greendev.global.exception.customException.MemberNotFoundException;
 import com.devoceanyoung.greendev.global.jwt.JwtProvider;
 import com.devoceanyoung.greendev.global.redis.RedisService;
 
@@ -25,14 +20,51 @@ import lombok.extern.slf4j.Slf4j;
 @Transactional
 @RequiredArgsConstructor
 public class AuthService {
-	final private MemberRepository memberRepository;
-	final private PasswordEncoder passwordEncoder;
-	final private JwtProvider jwtProvider;
-	final private RedisService redisService;
 
+	private final JwtProvider jwtProvider;
+	private final RedisService redisService;
 
-	private final AuthenticationManagerBuilder authenticationManagerBuilder;
+	public AccessTokenDto signOut(AccessTokenDto requestDto) {
+		// accessToken에서 Authentication 추출하기
+		String accessToken = requestDto.getAccessToken();
+		Authentication authentication = jwtProvider.getAuthentication(accessToken);
 
+		// Redis의 RefreshToken을 가져오면서, 이미 로그아웃된 사용자인 경우 예외 처리
+		String refreshToken = redisService.getRefreshToken(authentication.getName())
+			.orElseThrow(() -> new RuntimeException("RefreshToken NotFound:" + authentication.getName()));
+
+		// AccessToken의 남은 시간 추출 후 BlackList에 저장
+		Long remainingTime = jwtProvider.getRemainingTime(accessToken);
+		redisService.setData("BlackList:" + accessToken, "signOut", remainingTime);
+		redisService.deleteData("RefreshToken:" + authentication.getName());
+
+		return new AccessTokenDto(accessToken);
+	}
+
+	public AccessTokenDto refresh(String refreshToken) {
+		// 들어온 refreshToekn 검증
+		if (!jwtProvider.validateToken(refreshToken)) {
+			log.error("유효하지 않은 토큰입니다. {}", refreshToken);
+			throw new RuntimeException("Refresh Token 검증에 실패한 토큰입니다. : " + refreshToken);
+		}
+		// refreshToken에서 Authentication 추출하기
+		Authentication authentication = jwtProvider.getAuthentication(refreshToken);
+
+		// Redis의 RefreshToken을 가져오면서, 로그아웃된 사용자인 경우 예외 처리
+		String findRefreshToken = redisService.getRefreshToken(authentication.getName())
+			.orElseThrow(() -> new RuntimeException(authentication.getName()));
+
+		// 저장되어있던 refreshToken과 일치하는지 확인
+		if (!refreshToken.equals(findRefreshToken)) {
+			log.error("저장된 토큰과 일치하지 않습니다. {} {}", refreshToken, findRefreshToken);
+			throw new RuntimeException("Refresh Token 저장된 토큰과 일치하지 않습니다. : " + refreshToken);
+		}
+
+		// 토큰 생성을 위해 accessToken에서 Claims 추출
+		String newAccessToken = jwtProvider.generateAccessToken(authentication.getName());
+
+		return new AccessTokenDto(newAccessToken);
+	}
 
 
 	public UsernamePasswordAuthenticationToken getAuthenticationToken(String email, String password) {
