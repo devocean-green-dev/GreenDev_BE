8b873ee399922fbc089fe644ab25a60abd9c7310
june0216 <ziyun1612@ewhain.net>
2023-07-29 03:08:34 +0900

    Update ResponseConstant : 응답 메시지 상수화

--- greendev/src/main/java/com/devoceanyoung/greendev/global/constant/ResponseConstant.java
+++ greendev/src/main/java/com/devoceanyoung/greendev/global/constant/ResponseConstant.java
@@ -2,4 +2,6 @@ package com.devoceanyoung.greendev.global.constant;
 
 public class ResponseConstant {
 	public static final String MEMBER_NOT_FOUND = "해당 계정을 찾을 수 없습니다.";
+	public static final String NICKNAME_CHANGE_SUCCESS = "성공적으로 닉네임이 변경되었습니다.";
+
 }
