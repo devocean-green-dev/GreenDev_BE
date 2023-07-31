f72c2685db79d5bbeffb5e14e98ab18703827527
june0216 <ziyun1612@ewhain.net>
2023-07-31 17:06:31 +0900

    Update NaverUserInfo : 오타 수정

--- greendev/src/main/java/com/devoceanyoung/greendev/domain/auth/domain/NaverUserInfo.java
+++ greendev/src/main/java/com/devoceanyoung/greendev/domain/auth/domain/NaverUserInfo.java
@@ -13,7 +13,7 @@ public class NaverUserInfo implements OAuth2UserInfo{
 
 	private static final String PROVIDER_ID = "id";
 	private static final String EMAIL = "email";
-	private static final String NICKNAME = "name";
+	private static final String NICKNAME = "nickname";
 	private static final String PROFILE = "profile_image";
 
 	private final Map<String, Object> attributes;
@@ -53,7 +53,8 @@ public class NaverUserInfo implements OAuth2UserInfo{
 
 
 	public String getNickname() {
-		return attributes.get(NICKNAME).toString();
+		Object nicknameObj = attributes.get(NICKNAME);
+		return nicknameObj == null ? null : nicknameObj.toString();
 	}
 	@Override
 	public String getProfileImageUrl(){
