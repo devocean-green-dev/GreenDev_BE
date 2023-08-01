46fce648833abaf5795d4cbbb7982edab3ae6bc3
june0216 <ziyun1612@ewhain.net>
2023-08-02 01:44:26 +0900

    Update PostRepository : 로그인한 계정의 최근 30일 post 조회, 잔디(참여이력) 조회

--- greendev/src/main/java/com/devoceanyoung/greendev/domain/post/repository/PostRepository.java
+++ greendev/src/main/java/com/devoceanyoung/greendev/domain/post/repository/PostRepository.java
@@ -1,15 +1,25 @@
 package com.devoceanyoung.greendev.domain.post.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devoceanyoung.greendev.domain.campaign.domain.Campaign;
+import com.devoceanyoung.greendev.domain.member.domain.Member;
 import com.devoceanyoung.greendev.domain.post.domain.Post;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 	Page<Post> findByCampaign(Campaign campaign, Pageable pageable);
+
 	List<Post> findAllByCampaign(Campaign campaign);
+
+	@Query("SELECT p FROM Post p WHERE p.writer = :member AND p.date BETWEEN :startDate AND :endDate")
+	List<Post> findByWriterAndDateBetween(Member member, LocalDateTime startDate, LocalDateTime endDate);
+	@Query("SELECT function('DATE', p.date) as date, COUNT(p) as count FROM Post p WHERE p.writer = :member AND p.date BETWEEN :startDate AND :endDate GROUP BY function('DATE', p.date)")
+	List<Object[]> countPostsByMemberAndDateBetween(Member member, LocalDateTime startDate, LocalDateTime endDate);
+
 }
