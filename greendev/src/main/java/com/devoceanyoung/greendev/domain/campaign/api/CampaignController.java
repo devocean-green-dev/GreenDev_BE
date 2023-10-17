1f9b9fb6cd08631b42cdd1cc6d6d8661a9ff00ee
kwonjiyun <ziyun1612@ewhain.net>
2023-10-18 02:53:30 +0900

    Update CampaignController.java : 검색 api 추가

--- greendev/src/main/java/com/devoceanyoung/greendev/domain/campaign/api/CampaignController.java
+++ greendev/src/main/java/com/devoceanyoung/greendev/domain/campaign/api/CampaignController.java
@@ -9,15 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devoceanyoung.greendev.domain.auth.AuthUser;
@@ -117,6 +109,19 @@ public class CampaignController {
 			.message(StatusEnum.OK.getCode())
 			.data(CAMPAIGN_DELETE_SUCCESS)
 			.build());
+	}
+
+	@GetMapping("/search")
+	public ResponseEntity<StatusResponse> searchCampaignList(
+			@RequestParam String content,
+			Pageable pageable) {
+		Page<Campaign> campaignList = campaignService.searchCampaign(content, pageable);
+		CampaignResDto response = CampaignResDto.of(campaignList);
+		return ResponseEntity.ok(StatusResponse.builder()
+				.status(StatusEnum.OK.getStatusCode())
+				.message(StatusEnum.OK.getCode())
+				.data(response)
+				.build());
 	}
 
 
