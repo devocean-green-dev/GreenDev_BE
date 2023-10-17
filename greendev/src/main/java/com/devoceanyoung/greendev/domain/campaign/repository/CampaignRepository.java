be60d65510a7cedc357c60858ef94dd8b8e456a6
kwonjiyun <ziyun1612@ewhain.net>
2023-10-18 02:53:58 +0900

    Update CampaignRepository.java : 검색 쿼리 메서드 추가

--- greendev/src/main/java/com/devoceanyoung/greendev/domain/campaign/repository/CampaignRepository.java
+++ greendev/src/main/java/com/devoceanyoung/greendev/domain/campaign/repository/CampaignRepository.java
@@ -7,8 +7,16 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devoceanyoung.greendev.domain.campaign.domain.Campaign;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CampaignRepository extends JpaRepository<Campaign, Long> {
 	Optional<Campaign> findById(Long campaignId);
 	Page<Campaign> findAll(Pageable pageable);
+	@Query("SELECT c FROM Campaign c WHERE "
+			+ "LOWER(c.title) LIKE LOWER(CONCAT('%', :searchString, '%')) OR "
+			+ "LOWER(c.category) LIKE LOWER(CONCAT('%', :searchString, '%')) OR "
+			+ "LOWER(c.description) LIKE LOWER(CONCAT('%', :searchString, '%'))")
+	Page<Campaign> searchCampaigns(String searchString, Pageable pageable);
+
+
 }
