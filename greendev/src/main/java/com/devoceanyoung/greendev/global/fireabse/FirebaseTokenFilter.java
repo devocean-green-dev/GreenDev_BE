6af66b56a9cf9d5f0092ed428eb306b1739dd3eb
kwonjiyun <ziyun1612@ewhain.net>
2023-11-02 21:57:21 +0900

    Added FirebaseTokenFilter.java : 토큰 필터 설정

--- greendev/src/main/java/com/devoceanyoung/greendev/global/fireabse/FirebaseTokenFilter.java
+++ greendev/src/main/java/com/devoceanyoung/greendev/global/fireabse/FirebaseTokenFilter.java
@@ -16,8 +16,6 @@ public class FirebaseTokenFilter {
         try {
             return FirebaseAuth.getInstance().verifyIdToken(idToken);
         } catch (FirebaseAuthException e) {
-            // 여기에서 더 구체적인 예외 처리를 할 수 있습니다.
-            // 예를 들어, 사용자 정의 예외를 던질 수 있습니다.
             throw new TokenVerificationException("firebase Token is not valid", e);
         }
     }
