0f9c6f1b7d71ddc70f6e407e8b6de84a5139b826
kwonjiyun <ziyun1612@ewhain.net>
2023-11-02 21:01:15 +0900

    Update AuthService.java : 파이어베이스 인증토큰을 받아 시큐리티 컨텍스트에 추가하고 엑세스 토큰 발급

--- greendev/src/main/java/com/devoceanyoung/greendev/domain/auth/service/AuthService.java
+++ greendev/src/main/java/com/devoceanyoung/greendev/domain/auth/service/AuthService.java
@@ -1,8 +1,18 @@
 package com.devoceanyoung.greendev.domain.auth.service;
 
+import com.devoceanyoung.greendev.domain.auth.domain.GoogleUserInfo;
+import com.devoceanyoung.greendev.domain.auth.dto.FirebaseAuthToken;
 import com.devoceanyoung.greendev.domain.auth.dto.RefreshTokenResDto;
+import com.devoceanyoung.greendev.domain.auth.dto.TokenResDto;
+import com.devoceanyoung.greendev.domain.member.domain.Member;
+import com.devoceanyoung.greendev.domain.member.domain.ProviderType;
+import com.devoceanyoung.greendev.domain.member.domain.RoleType;
+import com.devoceanyoung.greendev.domain.member.repository.MemberRepository;
+import com.devoceanyoung.greendev.global.fireabse.FirebaseTokenFilter;
+import com.google.firebase.auth.FirebaseToken;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
 
 import org.springframework.stereotype.Service;
@@ -24,6 +34,9 @@ public class AuthService {
 
 	private final JwtProvider jwtProvider;
 	private final RedisService redisService;
+	private final FirebaseTokenFilter fireBaseFilter;
+	private final AuthenticationManagerBuilder authenticationManagerBuilder;
+	private final MemberRepository memberRepository;
 
 	public AccessTokenDto signOut(AccessTokenDto requestDto) {
 		// accessToken에서 Authentication 추출하기
@@ -81,5 +94,48 @@ public class AuthService {
 				.orElseThrow(() -> new RuntimeException("RefreshToken NotFound:" + authentication.getName()));
 		Long remainingTime = jwtProvider.getRemainingTime(refreshToken);
 		return new RefreshTokenResDto(refreshToken, remainingTime);
+	}
+
+    public TokenResDto login(FirebaseAuthToken firebaseAuthToken) {
+		FirebaseToken firebaseToken = fireBaseFilter.verifyToken(firebaseAuthToken.getFirebaseAuthToken());
+		if(!isExistedEmail(firebaseToken.getEmail())){
+			signUp(firebaseToken);
+		}
+		UsernamePasswordAuthenticationToken authenticationToken = getAuthenticationToken(
+				firebaseToken.getEmail(), firebaseToken.getUid());
+		Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+		String email = authentication.getName();
+
+		String accessToken = jwtProvider.generateAccessToken(email);
+		String refreshToken = jwtProvider.generateRefreshToken(email);
+		Long refreshTokenValidationMs = jwtProvider.getRefreshTokenValidationMs();
+
+		redisService.setData("RefreshToken:" + authentication.getName() , refreshToken, refreshTokenValidationMs);
+		return TokenResDto.builder()
+				.accessToken(accessToken)
+				.refreshToken(refreshToken)
+				.refreshTokenRemaininTime(refreshTokenValidationMs)
+				.build();
+    }
+
+	private void signUp(FirebaseToken firebaseToken) {
+		String password = firebaseToken.getName() + "_"+ firebaseToken.getEmail();
+		String username = "google" + "_" +firebaseToken.getUid();
+		Member member = Member.builder()
+				.nickname(firebaseToken.getName())
+				.email(firebaseToken.getEmail())
+				.password(password)
+				.profileImageUrl(firebaseToken.getPicture())
+				.username(username)
+				.providerType(ProviderType.GOOGLE)
+				.roleType(RoleType.USER)
+				.build();
+		memberRepository.save(member);
+	}
+
+	@Transactional(readOnly = true)
+	public boolean isExistedEmail(String email){
+		return memberRepository.existsByEmail(email);
 	}
 }
